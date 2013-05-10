f5c34ed02da09a7d90131be015e8974c48c2b8a1
Dane Bills <bills442@yahoo.com>
2013-05-09 22:33:47 -0400

    stupid maze utility rewrote

--- mz.pl
+++ mz.pl
@@ -1,50 +1,32 @@
 #generate maze bitstring
 # col * rows at two bytes, + line ending characters
-$sz = 22*23*2+(23);
-$bytes_read = read(STDIN,$MAZE,$sz);
-$bytes_read==$sz || die("incorrect maze size\n");
-print "bytes read $bytes_read\n";
-#print "mz = $MAZE\n";
-$MAZE=~s/\n//g;
-#print "mz = $MAZE\n";
-$output="";
-for($i=0;$i<length($MAZE);$i+=2) 
-{
-    $char = substr($MAZE,$i,1);
-    if($char eq "#") {
-        $output=$output."001";
-    } elsif($char eq ".") {
-        $output=$output."010";
-    } elsif($char eq ">") {
-        $output=$output."010";
-    } elsif($char eq "<") {
-        $output=$output."011";
-    } elsif($char eq "^") {
-        $output=$output."100";
-    } elsif($char eq "-") {
-        $output=$output."101";
-    } elsif($char eq "=") {
-        $output=$output."110";
-    } elsif($char eq "|") {
-        $output=$output."111";
-    } elsif($char eq " ") {
-        $output=$output."000";
+use Switch;
+@LINES=<STDIN>;
+chop(@LINES);
+chop(@LINES);
+foreach(@LINES) {
+    $len = length($_);
+    $len==22 || die("line length wrong $len:\n$_\n");
+    for($i=0;$i<$len;$i++) {
+        $char=substr($_,$i,1);
+        switch($char) {
+            case "#" { $output=$output."001" }
+            case "." { $output=$output."010" }
+            case "*" { $output=$output."011" }
+            case " " { $output=$output."000" }
+            else { die("huh?"); }
+        }
     }
 }
-print "MazeB\n"; 
-print "    dc.b 1,%";
+$len = length($output);
+print "len = $len\n";
+$output=$output."00";
+print "MazeB\n";
 for($i=0;$i<length($output);$i++) {
-    if($i%8==0 && $i!=0) {
-        print "\n    dc.b 1,%";
+    if($i%8==0) {
+        print "\n    dc.b %";
     }
     print substr($output,$i,1);
+        
 }
-print "\n";
-# ;; DOT
-# ;; POWER
-# ;; LEFT
-# ;; RIGHT
-# ;; TOP
-# ;; BOTTOM
-# ;; H WALL
-# ;; V WALL
+print "\n$output\n";
